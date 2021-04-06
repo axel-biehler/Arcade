a844ead273e3d37b4a39321abf1b08392a55d008
Maxime <max@desktop-mtbnc41.home>
2021-04-06 11:08:11 +0200

    add map key

--- src/libSDL/LibSDL.hpp
+++ src/libSDL/LibSDL.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <map>
 #include "../../includes/IGraphic.hpp"
 
 #define SDL_BLUE SDL_Color{0, 0, 255, 255}
@@ -30,5 +31,58 @@ namespace Arcade {
         private:
             SDL_Window *_window;
             SDL_Renderer *_renderer;
+    };
+
+    std::map<int, CommandType> SDL_key {
+        {SDLK_a, A},
+        {SDLK_b, B},
+        {SDLK_c, C},
+        {SDLK_d, D},
+        {SDLK_e, E},
+        {SDLK_f, F},
+        {SDLK_g, G},
+        {SDLK_h, H},
+        {SDLK_i, I},
+        {SDLK_j, J},
+        {SDLK_k, K},
+        {SDLK_l, L},
+        {SDLK_m, M},
+        {SDLK_n, N},
+        {SDLK_o, O},
+        {SDLK_p, P},
+        {SDLK_q, Q},
+        {SDLK_r, R},
+        {SDLK_s, S},
+        {SDLK_t, T},
+        {SDLK_u, U},
+        {SDLK_v, V},
+        {SDLK_w, W},
+        {SDLK_x, X},
+        {SDLK_y, Y},
+        {SDLK_z, Z},
+        {SDLK_0, NUM0},
+        {SDLK_1, NUM1},
+        {SDLK_2, NUM2},
+        {SDLK_3, NUM3},
+        {SDLK_4, NUM4},
+        {SDLK_5, NUM5},
+        {SDLK_6, NUM6},
+        {SDLK_7, NUM7},
+        {SDLK_8, NUM8},
+        {SDLK_9, NUM9},
+        {SDLK_LEFT, LEFT},
+        {SDLK_RIGHT, RIGHT},
+        {SDLK_UP, UP},
+        {SDLK_DOWN, DOWN},
+        {SDLK_KP_ENTER, ENTER},
+        {SDLK_SPACE, SPACE},
+        {SDLK_BACKSPACE, BACKSPACE},
+        {SDLK_ESCAPE, ESC},
+        {SDLK_F1, F1},
+        {SDLK_F2, F2},
+        {SDLK_F3, F3},
+        {SDLK_F4, F4},
+        {SDLK_F5, F5},
+        {SDLK_F6, F6}
     };
 }
