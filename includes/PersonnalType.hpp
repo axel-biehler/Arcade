219ad54d3d9bdf3229a88c61046eb5bf1526cae1
Maxime <max@desktop-mtbnc41.home>
2021-04-06 10:05:40 +0200

    change enum

--- includes/PersonnalType.hpp
+++ includes/PersonnalType.hpp
@@ -8,15 +8,60 @@
 #ifndef B_OOP_400_STG_4_1_ARCADE_NICOLAS_SCHNEIDER_PERSONNALTYPE_HPP
 #define B_OOP_400_STG_4_1_ARCADE_NICOLAS_SCHNEIDER_PERSONNALTYPE_HPP
 
-namespace Arcade {
-    enum CommandType {
-        ESCAPE,
-        KEYDOWN,
-        KEYUP,
-        SPACE,
+enum CommandType {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+        NUM0,
+        NUM1,
+        NUM2,
+        NUM3,
+        NUM4,
+        NUM5,
+        NUM6,
+        NUM7,
+        NUM8,
+        NUM9,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
         ENTER,
-        RESIZE,
-        NONE
+        SPACE,
+        BACKSPACE,
+        DELETE,
+        ESC,
+        F1,
+        F2,
+        F3,
+        F4,
+        F5,
+        F6,
+        NO_EVENT,
+        CLOSE_WINDOW
     };
 
     enum LibType {
@@ -26,9 +71,15 @@ namespace Arcade {
     };
 
     enum Color {
-        BLUE,
+        BLACK,
+        WHITE,
         RED,
-        WHITE
+        GREEN,
+        BLUE,
+        YELLOW,
+        MAGENTA,
+        CYAN,
+        TRANSPARENT
     };
 
     enum MaxWindow {
